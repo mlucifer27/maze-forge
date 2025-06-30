2865b4e990b3d7bb5a7363d2ab65256f0beb9bc8
mlucifer27 <nexgen.luciferr27@gmail.com>
2025-06-30 05:35:09 -0500

    u: update README - deployment URL

--- README.md
+++ README.md
@@ -1,14 +1,11 @@
 ![Alt text](public/android-chrome-192x192.png)
-# Amaze.me
+
+# Maze Forge
 
 A maze generator and three-dimensional visualizer for the web.
 
-Amaze.me is a web app that generates random mazes and visualizes them in 3D. It is written in TypeScript, uses the Three.js library for rendering and Mui-Joy for its UI.
+Maze Forge is a web app that generates random mazes and visualizes them in 3D. It is written in TypeScript, uses the Three.js library for rendering and Mui-Joy for its UI.
 
 ## Usage
 
-The app is hosted at [https://amaze.me.bsodium.fr/](https://amaze.me.bsodium.fr/). You can also download the source code and run it locally. The app is written in TypeScript React and uses [Joy-UI](https://mui.com/) for the UI.
-
-## License
-
-This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+The app is hosted at [https://maze-forge.vercel.app/](https://maze-forge.vercel.app/). You can also download the source code and run it locally. The app is written in TypeScript React and uses [Joy-UI](https://mui.com/) for the UI.
--- src/pages/Simulation.tsx
+++ src/pages/Simulation.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import { Box, Button, Link } from '@mui/joy';
+import React from "react";
+import { Box, Button, Link } from "@mui/joy";
 
-import { FiGithub } from 'react-icons/fi';
-import MazeView from '../components/MazeView';
-
+import { FiGithub } from "react-icons/fi";
+import MazeView from "../components/MazeView";
 
 export default function Simulation() {
   return (
     <Box
       component="div"
       sx={{
-        position: 'relative',
-        width: '100vw',
-        height: '100vh',
+        position: "relative",
+        width: "100vw",
+        height: "100vh",
       }}
     >
       <Link
-        href="https://github.com/BSoDium/amaze.me/fork"
+        href="https://github.com/mlucifer27/maze-forge/fork"
         underline="none"
         target="_blank"
         sx={{ position: "fixed", bottom: 20, right: 20, zIndex: 1000 }}
       >
         <Button
           color="neutral"
-          variant='soft'
-          size='lg'
+          variant="soft"
+          size="lg"
           startDecorator={<FiGithub />}
           sx={{ boxShadow: "md", borderRadius: 9999 }}
         >
